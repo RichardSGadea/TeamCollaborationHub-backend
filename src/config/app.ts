e8ec3fb98e3a7a56215aea9d526a874e2e2f0e84
Richard <richardsgadea@gmail.com>
2024-05-14 15:16:42 +0200

    feat: Added minimum configuration of routes

--- src/config/app.ts
+++ src/config/app.ts
@@ -3,11 +3,19 @@
 import express,{ Application } from "express";
 import cors from "cors";
 import { corsOptions } from "./cors";
+import apiRoutes from "../routes/api.routes"
+import baseRoutes from "../routes/base.routes"
 
 
 const app:Application= express();
 
 app.use(express.json());
 app.use(cors(corsOptions));
+
+//Register BASE route
+app.use("/", baseRoutes);
+
+//Register API routes
+app.use("/api", apiRoutes);
 
 export default app;
---
+++ src/routes/api.routes.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+
+const router = express.Router();
+
+//API routes
+
+
+export default router;
